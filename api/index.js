131a71ab945bfc5ebfaac1511caeb0ec7c01a40f
pranavgugale561-prog <pranavgugale561@gmail.com>
2026-09-03 01:44:23 +0530

    chore: add error reporting to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,6 @@
-const app = require('../server/index.js');
-module.exports = app;
+try {
+  const app = require('../server/index.js');
+  module.exports = app;
+} catch (e) {
+  module.exports = (req, res) => res.status(500).json({ error: e.message, stack: e.stack });
+}
